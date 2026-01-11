9411a87ce7cedbd61c5b1c2b1cffd40ffbcb2081
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-01-11 21:11:38 +0530

    Add solution for reversing a linked list
    Implements a function to reverse a singly linked list and return the new head. Includes example inputs and explanations in comments.

---
+++ Day 012/Reverse a linked list.py
@@ -0,0 +1,40 @@
+# Reverse a linked list
+
+# You are given the head of a singly linked list. You have to reverse the linked list and return the head of the reversed list.
+
+# Examples:
+
+# Input:
+      
+# Output: 4 -> 3 -> 2 -> 1
+# Explanation: After reversing the linkedList
+      
+# Input: 
+      
+# Output: 8 -> 9 -> 10 -> 7 -> 2
+# Explanation: After reversing the linked list
+      
+# Input: 
+      
+# Output: 8
+# Explanation:
+
+
+"""
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+"""
+
+class Solution:
+    def reverseList(self, head):
+        # Code here
+        prev = None
+        current = head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        return prev
