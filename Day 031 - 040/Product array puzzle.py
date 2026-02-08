47bfbbd6a59f7b7fd381c50eff8f8a08cb82f170
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-02-09 00:16:53 +0530

    Create Product array puzzle.py

---
+++ Day 031 - 040/Product array puzzle.py
@@ -0,0 +1,34 @@
+# Product array puzzle
+# Difficulty: EasyAccuracy: 33.46%Submissions: 306K+Points: 2Average Time: 20m
+# Given an array, arr[] construct a product array, res[] where each element in res[i] is the product of all elements in arr[] except arr[i]. Return this resultant array, res[].
+# Note: Each element is res[] lies inside the 32-bit integer range.
+
+# Examples:
+
+# Input: arr[] = [10, 3, 5, 6, 2]
+# Output: [180, 600, 360, 300, 900]
+# Explanation: For i=0, res[i] = 3 * 5 * 6 * 2 is 180.
+# For i = 1, res[i] = 10 * 5 * 6 * 2 is 600.
+# For i = 2, res[i] = 10 * 3 * 6 * 2 is 360.
+# For i = 3, res[i] = 10 * 3 * 5 * 2 is 300.
+# For i = 4, res[i] = 10 * 3 * 5 * 6 is 900.
+# Input: arr[] = [12, 0]
+# Output: [0, 12]
+# Explanation: For i = 0, res[i] is 0.
+# For i = 1, res[i] is 12.
+
+arr= [10, 3, 5, 6, 2]
+ans=[]
+sum=1
+
+for i in range(len(arr)):
+    print(i)
+    arr=arr[1:]
+    print(arr)
+    for i in range(len(arr)):
+        sum=sum*arr[i]
+    ans.append(sum)
+    sum=1
+        
+        
+print(ans)
